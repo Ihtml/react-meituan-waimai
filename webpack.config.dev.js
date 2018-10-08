046b1e68498fe2ee9f0f01cf88a21413326bf48a
Jiang XiaoJun <2483690020@qq.com>
2018-10-08 23:00:26 +0800

    config webpack dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,32 @@
 const path = require('path')
+const fs = require('fs')
 const srcRoot = path.resolve(__dirname, './src')
 const devPath = path.resolve(__dirname, 'dev')
-const pagePath = path.resolve(srcRoot, 'pages')
+const pageDir = path.resolve(srcRoot, 'pages')
+const mainFile = 'index.js'
+// 寻找pages下的每个文件夹里的index.js作为入口文件
+function getEntry() {
+  let entryMap = {}
+  fs.readdirSync(pageDir).forEach((pathname) => {
+    let fullPathName = path.resolve(pageDir, pathname)
+    let stat = fs.statSync(fullPathName)
+    let fileName = path.resolve(fullPathName, mainFile)
 
+    if (stat.isDirectory() && fs.existsSync(fileName)) {
+      entryMap[pathname] = fileName
+    }
+  })
+  return entryMap
+}
+
+const entryMap = getEntry()
 module.exports = {
-  entry: {},
-  output: {},
+  mode: 'development',
+  entry: entryMap,
+  output: {
+    path: devPath,
+    filename: '[name].min.js'
+  },
   // 加载器配置
   module: {
     rules: [
