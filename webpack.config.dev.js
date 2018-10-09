c77d4796a01e3fb38ecb2c92f3ec5f3a89a28640
IFE2 <2483690020@qq.com>
2018-10-09 16:12:05 +0800

    add config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,8 +5,25 @@ const srcRoot = path.resolve(__dirname, './src')
 const devPath = path.resolve(__dirname, 'dev')
 const pageDir = path.resolve(srcRoot, 'pages')
 const mainFile = 'index.js'
+
+function getHtmlArray (entryMap) {
+  let htmlArray = []
+  Object.keys(entryMap).forEach((key) => {
+    let fullPathName = path.resolve(pageDir, key)
+    let fileName = path.resolve(fullPathName, key + '.html')
+
+    if (fs.existsSync(filename)) {
+      htmlArray.push(new HtmlWebpackPlugin({
+        filename: key + '.html',
+        template: fileName,
+        chunks: [key]
+      }))
+    }
+  })
+  return htmlArray
+}
 // 寻找pages下的每个文件夹里的index.js作为入口文件
-function getEntry() {
+function getEntry () {
   let entryMap = {}
   fs.readdirSync(pageDir).forEach((pathname) => {
     let fullPathName = path.resolve(pageDir, pathname)
@@ -21,6 +38,7 @@ function getEntry() {
 }
 
 const entryMap = getEntry()
+const htmlArray = getHtmlArray(entryMap)
 module.exports = {
   mode: 'development',
   entry: entryMap,
@@ -38,6 +56,5 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin()
-  ]
+  ].concat(htmlArray)
 }
