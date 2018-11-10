114d5841e4d26cc61bd9189c1d21be257fde4173
Jiang XiaoJun <2483690020@qq.com>
2018-11-10 15:46:34 +0800

    add contentListActions

--- src/pages/index/actions/contentListAction.js
+++ src/pages/index/actions/contentListAction.js
@@ -1,1 +1,17 @@
-export const getListData = () => { }
+import { LIST_DATA } from './actionTypes'
+import axios from 'axios'
+
+export const getListData = (page) => (dispatch) => {
+  axios({
+    method: 'get',
+    url: './json/homelist.json'
+  }).then((resp) => {
+    window.setTimeout(() => {
+      dispatch({
+        type: LIST_DATA,
+        currentPage: page,
+        obj: resp.data
+      })
+    }, 1500)
+  })
+}
