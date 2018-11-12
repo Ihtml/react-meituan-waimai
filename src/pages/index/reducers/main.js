f4ca8446c253a4164b76c4c7743b31477abfe6b0
Jiang XiaoJun <2483690020@qq.com>
2018-11-12 23:37:40 +0800

    add contentListReducer

--- src/pages/index/reducers/main.js
+++ src/pages/index/reducers/main.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import tabReducer from './tabReducer'
 import categoryReducer from './categoryReducer'
+import contentListReducer from './contentListReducer.js'
 
 const reducers = combineReducers({
   tabReducer,
-  categoryReducer
+  categoryReducer,
+  contentListReducer
 })
 
 export default reducers
