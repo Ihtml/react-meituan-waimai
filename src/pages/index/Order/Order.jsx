148b3b5817ffe67f964a7c77720165cbad187cd3
Ihtml <2483690020@qq.com>
2019-04-13 21:51:31 +0800

    orderItem ScrollView

--- src/pages/index/Order/Order.jsx
+++ src/pages/index/Order/Order.jsx
@@ -3,15 +3,34 @@ import React from 'react'
 
 import { connect } from 'react-redux';
 import { getOrderData } from '../actions/orderAction';
+import ScrollView from 'component/ScrollView/ScrollView.jsx';
 
 import ListItem from './ListItem/ListItem';
 
 class Order extends React.Component {
   constructor(props) {
     super(props)
+    // 标示分页
     this.page = 0
+
+    // 是否还可以滚动加载
+    this.state = {
+      isend: false
+    };
     this.fetchData(this.page);
   }
+
+  loadPage() {
+    this.page++;
+    if (this.page > 3) {
+      this.setState({
+        isend: true
+      });
+    } else {
+      this.fetchData(this.page);
+    }
+  }
+
   fetchData(page) {
     this.props.dispatch(getOrderData(page));
   }
@@ -25,7 +44,9 @@ class Order extends React.Component {
     return (
       <div className='order'>
         <div className='header'>订单</div>
-        <div className="order-list">{this.renderList()}</div>
+        <ScrollView dis="order" loadCallback={this.loadPage.bind(this)} isend={this.state.isend}>
+          <div className="order-list">{this.renderList()}</div>
+        </ScrollView>
       </div>
     )
   }
