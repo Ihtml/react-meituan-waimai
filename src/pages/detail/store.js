bdb1c15111b5b28345fac8102f32a394b86326ed
Ihtml <2483690020@qq.com>
2019-05-25 23:03:15 +0800

    config detail store.js

--- src/pages/detail/store.js
+++ src/pages/detail/store.js
@@ -1,12 +1,30 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 
 import Reducer from './reducers/main'
-const store = createStore(Reducer)
+
+import thunk from 'redux-thunk';
 
 import createHistory from 'history/createHashHistory'
+import { routerMiddleware } from 'react-router-redux'
 
 // 创建基于hash的history
 const history = createHistory();
+
+// 创建初始化tab
+history.replace('menu');
+
+// 创建history的Middleware
+const historyMiddl = routerMiddleware(history);//, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+
+const store = createStore(Reducer, applyMiddleware(thunk, historyMiddl));
+
+
+if (module.hot) {
+  module.hot.accept('./reducers/main', () => {
+    const nextRootReducer = require('./reducers/main.js').default;
+    store.replaceReducer(nextRootReducer)
+  });
+}
 
 module.exports = {
   store,
